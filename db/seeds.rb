e20726123cb132c7f3eabefd074159d8c9c3748c
liangwen1217 <liangwen1217@gmail.com>
2018-07-27 17:28:27 +0800

    add user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+  10.times do |i|
+    user = User.create!(name: "user#{i+1}")
+    puts "#{user.name} created"
+  end
+
 file = File.open("#{Rails.root}/public/avatar/user#{rand(1..20)}.jpg")
 User.create(
   email: "aa123@gmail.com",
