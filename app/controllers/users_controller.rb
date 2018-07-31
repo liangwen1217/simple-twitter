8a1e1863ad4c44aae7ca39ec435b0ec2fe7b8e40
liangwen1217 <liangwen1217@gmail.com>
2018-07-31 23:25:21 +0800

    added followers page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :likes, :followings]
+  before_action :set_user, only: [:edit, :update, :likes, :followings, :]
 
   def tweets
     @user = User.find(params[:id])
@@ -17,11 +17,11 @@ class UsersController < ApplicationController
 
   def followings
     @followings # 基於測試規格，必須講定變數名稱
-    @followings = current_user.followings.all
+    @followings = @user.followings
   end
 
   def followers
-    @followers # 基於測試規格，必須講定變數名稱
+    @followers = @user.followers # 基於測試規格，必須講定變數名稱
   end
 
   def likes
