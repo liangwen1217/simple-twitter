a655bae5e89e8524a50b2f0b05541d682342d8bb
liangwen1217 <liangwen1217@gmail.com>
2018-08-05 23:13:09 +0800

    fixed admin::users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :likes, :followings, :]
+  before_action :set_user, only: [:tweets, :edit, :update, :likes, :followings, :]
 
   def tweets
-    @tweets = current_user.tweets.order(created_at: :desc)
+    @tweets = user.tweets.order(created_at: :desc)
   end
 
   def update
